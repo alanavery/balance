aa09a75d764ed83504ce2bd563fd6aeecc915c1b
delayedaa <delayedaa@gmail.com>
2021-02-09 13:14:32 -0600

    Install express and create server.js

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import { fruits } from './testData';
+
+import NewTransactionForm from './components/NewTransactionForm';
+import TransactionTable from './components/TransactionTable';
+
+function App() {
+  const [testData, setTestData] = useState(fruits);
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState('');
+  // const [transactions, setTransactions] = useState([]);
+
+  // const listTransactions = transactions.map((transaction, i) => {
+  //   return <p key={i}>{transaction}</p>;
+  // });
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const updatedTransactions = transactions;
+  //   updatedTransactions.push(`You paid ${payee} $${amount}.`);
+  //   setTransactions(updatedTransactions);
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Add this data to the table: ${name}, ${quantity}, and ${price}.`);
+    const newFruit = {
+      name: name,
+      quantity: Number(quantity),
+      price: price,
+    };
+    console.log(testData);
+    setTestData(testData.concat(newFruit));
+    console.log(testData);
+  };
+
+  return (
+    <div className="container">
+      <NewTransactionForm name={name} setName={setName} quantity={quantity} setQuantity={setQuantity} price={price} setPrice={setPrice} handleSubmit={handleSubmit} />
+      <TransactionTable testData={testData} />
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
--- src/components/Transaction.js
+++
@@ -1,9 +0,0 @@
-function Transaction(props) {
-  return (
-    <p>
-      You paid {props.payee} ${props.amount}
-    </p>
-  );
-}
-
-export default Transaction;
